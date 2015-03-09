d3ef0a2224428d7064285dc9b2a25bcf68f0fc67
mikaelbrandin <mikael.brandin@devolver.se>
2015-03-09 15:51:56 +0100

    New development of armory show modules

--- commands/show.py
+++ commands/show.py
@@ -3,7 +3,7 @@ __author__ = 'kra869'
 import os
 import os.path
 
-available_sections = ['mods', 'configs']
+available_sections = ['modules', 'mods', 'configuration', 'config']
 
 
 def init(context):
@@ -15,9 +15,9 @@ def init(context):
 
 def command_show(args, context):
     for section in args.sections:
-        if section == 'mods':
+        if section == 'mods' or section == 'modules':
             return command_show_mods(args, context)
-        elif section == 'configs':
+        elif section == 'configs' or section == 'configuration':
             return command_show_config(args, context)
 
     return None
@@ -25,11 +25,14 @@ def command_show(args, context):
 
 def command_show_mods(args, context):
     modules = {}
+
     for subdirectory in os.listdir(args.directory + '/modules.d/'):
         modules[subdirectory] = context.modules.get(args.directory + '/modules.d/' + subdirectory)
 
+    print "{status:6} {name:50} {version:50}".format(name="Name", version="Version", status="Status")
+
     for name, module in modules.iteritems():
-        print module.name
+        print "[{status:^4}] {name:50} {version:50}".format(name=name, version=module.version, status="ok")
 
     return None
 
--- context.py
+++ context.py
@@ -22,15 +22,34 @@ class ReadWriteDirectory(argparse.Action):
             raise argparse.ArgumentError(self, "writable_dir:{0} is not a writable directory".format(prospective_dir))
 
 
+class ReadWriteRepositoryDirectory(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        prospective_dir = values
+
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentError(self, "ReadWriteRepositoryDirectory:{0} is not a valid path".format(prospective_dir))
+
+        if not os.access(prospective_dir, os.R_OK):
+            raise argparse.ArgumentError(self, "ReadWriteRepositoryDirectory:{0} is not a readable directory".format(prospective_dir))
+
+        if not os.path.exists(prospective_dir + os.path.sep + '.armory'):
+            raise argparse.ArgumentError(self, "ReadWriteRepositoryDirectory:{0} is not a Armory".format(prospective_dir))
+
+        if os.access(prospective_dir, os.W_OK):
+            setattr(namespace, self.dest, prospective_dir)
+        else:
+            raise argparse.ArgumentError(self, "ReadWriteRepositoryDirectory:{0} is not a writable directory".format(prospective_dir))
+
+
 class Context:
     def __init__(self):
         current_dir = os.getcwd()
 
         self.modules = Modules()
 
-        self.args_parser = argparse.ArgumentParser(prog='Armory');
+        self.args_parser = argparse.ArgumentParser(prog='Armory')
         self.args_parser.add_argument('--debug', action='store_true', help='Enable debugging (mainly)')
-        self.args_parser.add_argument('--directory', metavar='FILE', action=ReadWriteDirectory, default=current_dir)
+        self.args_parser.add_argument('--directory', metavar='FILE', action=ReadWriteRepositoryDirectory, default=current_dir)
         self.sub_args_parsers = self.args_parser.add_subparsers(title='Armory commands', description='The commands available with Armory', help='Available Armory commmands')
 
     def register_command(self, cmd, command, **kwargs):
@@ -67,8 +86,9 @@ class Module:
         self.config = ConfigParser.SafeConfigParser()
         self.config.read(self.module_info_file)
 
-        self.version = self.config.get('general', 'version');
-        // self.name = self.config.has_option('general', 'name')
+        self.version = self.config.get('general', 'version')
+        if self.config.has_option('general', 'name'):
+            self.name = self.config.get('general', 'name')
 
         return True
 
