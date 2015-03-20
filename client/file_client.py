27c33ab693005816d34850ec1129dae7877b5440
mikaelbrandin <mikael.brandin@devolver.se>
2015-03-20 16:24:48 +0100

    SSH, starting out

--- client/file_client.py
+++ client/file_client.py
@@ -1,71 +1,75 @@
 __author__ = 'kra869'
 
 import subprocess
-import protocol as proc
 import urlparse
+
+import protocol as proc
+
 
 class TimeoutException(BaseException):
     def __init__(self):
         pass
-        
+
 
 class Process:
     def __init__(self, cmd, cwd):
         self.process = subprocess.Popen([cmd], cwd=cwd, bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        
+
     def read_msg(self):
         return proc.read_msg(self.process.stdout);
-            
+
     def write_msg(self, msg):
         return proc.write_msg(self.process.stdin, msg)
-        
-    def write_file(self,file, hash):
+
+    def write_file(self, file, hash):
         return proc.write_file(self.process.stdin, file, hash)
-        
+
     def read_file(self, file, hash):
         return proc.read_file(self.process.stdout, file, hash)
-        
+
     def wait(self):
         self.process.wait();
+
 
 class Client:
     def __init__(self, uri):
         self.uri = urlparse.urlparse(uri)
+        self.cmd = 'armory-push'
         pass
-        
+
     def push(self, packageName, packageFile, packageHash):
-          
-        p = Process('armory-push', self.uri.path);
-       
+
+        p = Process(self.cmd, self.uri.path)
+
         msg = p.read_msg();
         p.write_msg("push {package} {hash}".format(package=packageName, hash=packageHash))
         p.write_msg("ok");
-        
+
         msg = p.read_msg();
-        
+
         if msg.msg == 'accept':
-            print "-> "+packageName
+            print "-> " + packageName
             p.write_file(packageFile, packageHash)
             p.wait()
             return None
         elif msg.msg == 'reject':
-            print "Rejected "+packageName
-            return  None
+            print "Rejected " + packageName
+            return None
         elif msg.msg == 'error':
-            print "Error "+packageName
-            return  None
+            print "Error " + packageName
+            return None
         else:
             print "oops"
             return None
-            
+
     def pull(self, module, version, dst):
-        
+
         p = Process('armory-pull', self.uri.path);
-        
+
         msg = p.read_msg();
-        p.write_msg("pull /modules/"+module+'/'+version);
+        p.write_msg("pull /modules/" + module + '/' + version);
         p.write_msg("ok");
-        
+
         msg = p.read_msg();
         if msg.msg == 'accept':
             p.read_file(dst, 0)
@@ -73,10 +77,10 @@ class Client:
             return None
         elif msg.msg == 'reject':
             print "Rejected"
-            return  None
+            return None
         elif msg.msg == 'error':
             print "Error "
-            return  None
+            return None
         else:
             print "oops"
             return None
--- client/ssh_client.py
+++ client/ssh_client.py
@@ -1,10 +1,11 @@
 __author__ = 'kra869'
 
+import ssh_client as ssh
 
-class Client:
+
+class Client(ssh.Client):
     def __init__(self):
-        pass
-        
-    def push(self, packageName, packageFile):
+        super(Client, self)
+        self.cmd = "ssh {username}@{host} cd {path}; armory-push".format(username=self.uri.username, host=self.uri.netloc, path=self.uri.path)
         pass
         
