81aafc79d48147f85585f0f80dddc950dfe737a6
mikaelbrandin <mikael.brandin@devolver.se>
2015-03-29 21:10:38 +0200

    Added armory branch --set BRANCH

--- client/branch.py
+++ client/branch.py
@@ -11,11 +11,19 @@ class BranchException(exceptions.ArmoryException):
 
 def init(context):
     parser = context.register_command('branch', command_branch, aliases=['br'], help='Create, change and delete local branches')
-    #parser.add_argument('--branch', '-b', 
+    parser.add_argument('--set', '-s', metavar='BRANCH', help='switch to local branch BRANCH')
+    parser.add_argument('--info', '-i', action='store_true', help='show information about branches')
+ 
     #parser.add_argument('repository', metavar='URI', help='repository to checkout from')
    
 def command_branch(args, context):
+    if args.set:
+        move_to(context, args.set)
+        output.msgln("Changed branch to "+args.set, label='ok')
+    else:
+        command_branch_info(args, context)
 
+def command_branch_info(args, context):
     for branch_file in glob.iglob(context.home_directory + '*.armory'):
         name = os.path.basename(branch_file).split('.')[0]
         if name == context.branch_name:
